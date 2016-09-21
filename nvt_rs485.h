cd967bad6bf0fcaee6bdf2544d859d3cb20d3c97
wclin <devnull@localhost>
2016-09-21 03:58:51 +0000

    Fix for multi-platform.

--- nvt_rs485.h
+++ nvt_rs485.h
@@ -4,15 +4,8 @@
 #include "mbed.h"
 
 class NvtRS485 : public Serial {
-
 public:
-    enum E_PFM {
-        NUC472,
-        CNT
-    };
-
-    E_PFM m_ePFMID;
-    NvtRS485(PinName tx, PinName rx, PinName dir, E_PFM pfm=NUC472);
+    NvtRS485(PinName tx, PinName rx, PinName dir);
     virtual ~NvtRS485(void);
     int set_rs485_mode(PinName dir);
 };
